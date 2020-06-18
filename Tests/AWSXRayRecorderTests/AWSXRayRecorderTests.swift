aa73f50808a6c5b06177135f8e25d47e98f54d4b
Michal A <michal@pokryfka.net>
2020-06-18 22:18:07 +0800

    fix: update segment tests

--- Tests/AWSXRayRecorderTests/AWSXRayRecorderTests.swift
+++ Tests/AWSXRayRecorderTests/AWSXRayRecorderTests.swift
@@ -11,7 +11,7 @@ final class AWSXRayRecorderTests: XCTestCase {
         let testName = UUID().uuidString  // this is technically invalid trace id, not sure about dashes
         let testTraceId = UUID().uuidString  // this is technically invalid trace id
         let testparentId = UUID().uuidString
-        recorder.beginSubSegment(name: testName, traceId: testTraceId, parentId: testparentId)
+        _ = recorder.beginSubSegment(name: testName, traceId: testTraceId, parentId: testparentId)
 
         _ = recorder.sendSegments(emmiter: emmiter)
 
@@ -33,7 +33,7 @@ final class AWSXRayRecorderTests: XCTestCase {
         let testName = UUID().uuidString  // this is technically invalid trace id, not sure about dashes
         let testTraceId = UUID().uuidString  // this is technically invalid trace id
         let testparentId = UUID().uuidString
-        recorder.beginSubSegment(name: testName, traceId: testTraceId, parentId: testparentId)
+        _ = recorder.beginSubSegment(name: testName, traceId: testTraceId, parentId: testparentId)
         recorder.endSubSegment()
         _ = recorder.sendSegments(emmiter: emmiter)
 
@@ -52,32 +52,38 @@ final class AWSXRayRecorderTests: XCTestCase {
         let emmiter = MockEmmiter()
         let recorder = XRayRecorder()
 
-        let testName = UUID().uuidString  // this is technically invalid trace id, not sure about dashes
-        let testTraceId = UUID().uuidString  // this is technically invalid trace id
-        let testparentId = UUID().uuidString
-        recorder.beginSubSegment(name: testName, traceId: testTraceId, parentId: testparentId)
+        let testName = UUID().uuidString
+        let testTraceId = TraceId()
+        let segmentId = recorder.beginSubSegment(
+            name: testName,
+            traceId: String(describing: testTraceId),
+            parentId: nil)
 
-        let testName2 = UUID().uuidString  // this is technically invalid trace id, not sure about dashes
-        // TODO: check traceId and parentId
-        recorder.beginSubSegment(name: testName2, traceId: testTraceId, parentId: testparentId)
+        let testName2 = UUID().uuidString
+        _ = recorder.beginSubSegment(
+            name: testName2,
+            traceId: String(describing: testTraceId),
+            parentId: segmentId)
 
         _ = recorder.sendSegments(emmiter: emmiter)
 
         XCTAssertEqual(emmiter.documents.count, 1)
-        XCTAssertEqual(emmiter.documents.first?.count, 1)
 
-        let segment = emmiter.documents.first?.first
+        let segments = emmiter.documents.first
+        XCTAssertEqual(segments?.count, 1)
+
+        let segment = segments?.first
         XCTAssertNotNil(segment)
         XCTAssertEqual(segment?.name, testName)
-        XCTAssertEqual(segment?.traceId, testTraceId)
-        XCTAssertEqual(segment?.parentId, testparentId)
+        XCTAssertEqual(segment?.traceId, testTraceId.description)
+        XCTAssertEqual(segment?.parentId, nil)
         XCTAssertEqual(segment?.subsegments.count, 1)
 
         let subsegment = segment?.subsegments.first
         XCTAssertNotNil(subsegment)
         XCTAssertEqual(subsegment?.name, testName2)
-        XCTAssertEqual(subsegment?.traceId, testTraceId)
-        XCTAssertEqual(subsegment?.parentId, testparentId)
+        XCTAssertEqual(subsegment?.traceId, testTraceId.description)
+        XCTAssertEqual(subsegment?.parentId, segmentId)
         XCTAssertEqual(subsegment?.subsegments.count, 0)
     }
 
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -5,4 +5,5 @@ import XCTest
 var tests = [XCTestCaseEntry]()
 tests += HelloWorldTests.allTests()
 tests += AWSXrayRecorderTests.allTests()
+tests += AWSXRaySegmentTests.allTests()
 XCTMain(tests)
