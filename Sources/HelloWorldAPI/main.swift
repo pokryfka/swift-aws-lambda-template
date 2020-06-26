a61a4a30c4edb655eab228c7e82756bd0594e437
Michal A <michal@pokryfka.net>
2020-06-26 11:49:56 +0800

    refactor: use EventLoopLambdaHandler

--- Sources/HelloWorldAPI/main.swift
+++ Sources/HelloWorldAPI/main.swift
@@ -10,6 +10,8 @@ import class Foundation.JSONEncoder
 
 Backtrace.install()
 
+// TODO: simplify, see also https://github.com/swift-server/swift-aws-lambda-runtime/pull/129
+
 extension JSONDecoder {
     fileprivate func decode<T: Decodable>(type: T.Type, from string: String) throws -> T {
         try decode(type, from: Data(string.utf8))
@@ -42,10 +44,12 @@ private struct HelloWorldOut: Encodable {
     let message: Greeting
 }
 
-private let handler: Lambda.CodableClosure<APIGateway.Request, APIGateway.Response> = {
-    context, request, callback in
+// MARK: Using Closures
+
+#if false
+
+private let handler: Lambda.CodableClosure<APIGateway.Request, APIGateway.Response> = { context, request, callback in
     do {
-//        fatalError()
         let now = Date()
         let secondsFromGMT: Int
         if let body = request.body {
@@ -70,14 +74,63 @@ private let handler: Lambda.CodableClosure<APIGateway.Request, APIGateway.Respon
         )
         callback(.success(response))
     } catch let error as DecodingError {
-        context.logger.error("DecodingError: \(error.localizedDescription)")
+        context.logger.error("DecodingError: \(error)")
         let response = APIGateway.Response(statusCode: HTTPResponseStatus.badRequest)
         callback(.success(response))
     } catch {
-        context.logger.error("AnError: \(error.localizedDescription)")
+        context.logger.error("AnError: \(error)")
         let response = APIGateway.Response(statusCode: HTTPResponseStatus.internalServerError)
         callback(.success(response))
     }
 }
 
 Lambda.run(handler)
+
+#else
+
+import NIO
+
+private struct HelloWorldAPIHandler: EventLoopLambdaHandler {
+    typealias In = APIGateway.Request
+    typealias Out = APIGateway.Response
+
+    func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
+        do {
+            let now = Date()
+            let secondsFromGMT: Int
+            if let body = event.body {
+                let input = try jsonDecoder.decode(type: HelloWorldIn.self, from: body)
+                secondsFromGMT = input.secondsFromGMT
+            } else {
+                secondsFromGMT = 0
+            }
+            let greetingHour = try hour(onDate: now, inTimeZoneWithSecondsFromGMT: secondsFromGMT)
+            let greetingMessage = try greeting(atHour: greetingHour)
+            let output = HelloWorldOut(
+                now: now,
+                secondsFromGMT: secondsFromGMT,
+                hour: greetingHour,
+                message: greetingMessage
+            )
+            let body: String? = try jsonEncoder.encode(value: output)
+            let response = APIGateway.Response(
+                statusCode: HTTPResponseStatus.ok,
+                headers: ["Content-Type": "application/json"],
+                body: body
+            )
+            return context.eventLoop.makeSucceededFuture(response)
+        } catch let error as DecodingError {
+            context.logger.error("DecodingError: \(error)")
+            let response = APIGateway.Response(statusCode: HTTPResponseStatus.badRequest)
+            return context.eventLoop.makeSucceededFuture(response)
+        } catch {
+            context.logger.error("AnError: \(error)")
+            let response = APIGateway.Response(statusCode: HTTPResponseStatus.internalServerError)
+            return context.eventLoop.makeSucceededFuture(response)
+        }
+    }
+}
+
+Lambda.run(HelloWorldAPIHandler())
+
+#endif
--- Sources/HelloWorldScheduled/main.swift
+++ Sources/HelloWorldScheduled/main.swift
@@ -5,17 +5,47 @@ import HelloWorld
 
 Backtrace.install()
 
-private let handler: Lambda.CodableVoidClosure<Cloudwatch.ScheduledEvent> = {
-    context, _, callback in
+// MARK: Using Closures
+
+#if false
+
+private let handler: Lambda.CodableVoidClosure<Cloudwatch.ScheduledEvent> = { context, _, callback in
     do {
         let greetingHour = try hour()
         let greetingMessage = try greeting(atHour: greetingHour)
         context.logger.info("\(greetingMessage)")
         callback(.success(()))
     } catch {
-        context.logger.error("AnError: \(error.localizedDescription)")
+        context.logger.error("AnError: \(error)")
         callback(.failure(error))
     }
 }
 
 Lambda.run(handler)
+
+#else
+
+// MARK: Using EventLoopLambdaHandler
+
+import NIO
+
+private struct HelloWorldScheduledHandler: EventLoopLambdaHandler {
+    typealias In = Cloudwatch.ScheduledEvent
+    typealias Out = Void
+
+    func handle(context: Lambda.Context, event: In) -> EventLoopFuture<Out> {
+        do {
+            let greetingHour = try hour()
+            let greetingMessage = try greeting(atHour: greetingHour)
+            context.logger.info("\(greetingMessage)")
+            return context.eventLoop.makeSucceededFuture(Void())
+        } catch {
+            context.logger.error("AnError: \(error)")
+            return context.eventLoop.makeFailedFuture(error)
+        }
+    }
+}
+
+Lambda.run(HelloWorldScheduledHandler())
+
+#endif
