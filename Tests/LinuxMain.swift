26d944c259231e7307ff9c692a5bbe50460ede7e
pokryfka <dev@pokryfka.net>
2020-08-06 10:00:31 +0800

    chore: update workflows (#16)
    * chore: move lint to its own workflow
    
    * chore: create documentation workflow
    
    Closes #13
    
    * chore: rename build workflow to test, enable tests discovery on linux
    
    * docs: describe GitHub Actions in README

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,8 +1,1 @@
-import XCTest
-
-import HelloWorldTests
-
-var tests = [XCTestCaseEntry]()
-tests += HelloWorldTests.__allTests()
-
-XCTMain(tests)
+fatalError("Run the tests with `swift test --enable-test-discovery`.")
